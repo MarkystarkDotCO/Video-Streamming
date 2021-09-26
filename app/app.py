e2e77b575eff93961f10e5c92f95bce0d7f0aaf7
Methanon <meayrek17577@gmail.com>
2021-09-26 17:08:41 +0700

    MAIN PROJECT STRUCTURE

---
+++ app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+#from example_blueprint import example_blueprint
+from auth.auth import auth_bp
+from api.api import api_bp
+from payment.payment import payment_bp
+
+
+app = Flask(__name__)
+#app.register_blueprint(example_blueprint)
+app.register_blueprint(auth_bp, url_prefix='/auth')
+app.register_blueprint(api_bp, url_prefix='/api')
+app.register_blueprint(payment_bp, url_prefix='/payment')
+
+if __name__ == '__main__':
+  app.run(
+  #host='0.0.0.0', 
+  #port=80, 
+  debug=True)
