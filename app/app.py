27dc87daf1f475636de00fd316ccc65abf75a251
Methanon <meayrek17577@gmail.com>
2021-10-15 20:18:48 +0700

    add main.py

--- app/app.py
+++ app/app.py
@@ -3,13 +3,14 @@ from flask import Flask
 from auth.auth import auth_bp
 from api.api import api_bp
 from payment.payment import payment_bp
-
+from main.main import main_bp
 
 app = Flask(__name__)
 #app.register_blueprint(example_blueprint)
 app.register_blueprint(auth_bp, url_prefix='/auth')
 app.register_blueprint(api_bp, url_prefix='/api')
 app.register_blueprint(payment_bp, url_prefix='/payment')
+app.register_blueprint(main_bp, url_prefix='/')
 
 if __name__ == '__main__':
   app.run(
--- app/auth/auth.py
+++ app/auth/auth.py
@@ -3,11 +3,13 @@ import cgi
 
 auth_bp = Blueprint('auth', __name__, template_folder='templates', static_folder='static')
 
-
 @auth_bp.route('/')
 def login():
-    ID = cgi.FieldStorage()
-    id =  ID.getvalue('firstname')
-    print(id)
     return render_template('login.html')
 
+@auth_bp.route('/abc')
+def dog():
+    return "HELLO"
+#ID = cgi.FieldStorage()
+ #   id =  ID.getvalue('firstname')
+  #  print(id)
