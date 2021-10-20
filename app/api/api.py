814d34d6af1489efcb0fd2f518e3223cd358cf8c
Methanon <meayrek17577@gmail.com>
2021-10-21 01:27:21 +0700

    API push

--- app/api/api.py
+++ app/api/api.py
@@ -1,7 +1,25 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+#import psycopg2
+import pyodbc 
+
 
 api_bp = Blueprint('api', __name__)
 
+####### test API success
 @api_bp.route('/')
 def index():
-    return "api homepage"
+    server = 'webse.database.windows.net'
+    database = 'videostreaming'
+    username = 'webse001'
+    password = 'vajajava+25%'   
+    driver= '{ODBC Driver 17 for SQL Server}'
+    i = "HIIII"
+    with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT * FROM testDB")
+            row = cursor.fetchone()
+            while row:
+                print (str(row[0])+" ")
+                i= str(row[0])
+                row = cursor.fetchone()
+    return i
