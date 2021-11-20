d0655ee90a04325ab6472c804bbb535edb140591
62050251 <62050251@kmitl.ac.th>
2021-11-20 10:52:18 +0700

    connect to mssql database

--- app/main/main.py
+++ app/main/main.py
@@ -1,8 +1,19 @@
+
 from flask import Blueprint
+import pyodbc
 from flask.templating import render_template
 #git test
+
 main_bp = Blueprint('main', __name__, template_folder='templates', static_folder='static')
 
-@main_bp.route('/')
+@main_bp.route('/', methods = ['POST', 'GET'])
 def index():
-    return render_template('home.html')
+        conn = pyodbc.connect('Driver={SQL Server};'
+                              'Server=DESKTOP-NG2KJDA\SQLEXPRESS;'
+                              'Database=suki;'
+                              'Trusted_Connection=yes;')
+
+        cursor = conn.cursor()
+        cursor.execute('SELECT Url FROM Description')
+        data=cursor.fetchall()
+        return render_template('home.html',data=data)
